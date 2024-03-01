687454e4980c6b180028975671c4a6057c0b8da2
Subhajyoti Halder <75578800+subhu-darkknight72@users.noreply.github.com>
2024-03-01 18:59:49 +0530

    Delete backend directory

--- backend/src/config.ts
+++
@@ -1,2 +0,0 @@
-export const PORT = 8080;
-export const MONGO_URL = 'mongodb+srv://root:root@cluster0.m0oxt19.mongodb.net/?retryWrites=true&w=majority';
--- backend/src/router/index.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import hospitalRouter from './hospital.routes';
-
-const router = Router();
-router.use('/hospital', hospitalRouter);
-
-export default router;
