ac15acb3317ef9183810e121f75000c81f2ba788
Subhajyoti Halder <75578800+subhu-darkknight72@users.noreply.github.com>
2024-04-08 12:03:17 +0530

    Reset the repo

--- backend/hospital/admin.py
+++ backend/hospital/admin.py
@@ -1,9 +1,3 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import actionHistory
-
-class actionHistoryAdmin(admin.ModelAdmin):
-    list_display = ('action', 'observation')
-
-admin.site.register(actionHistory, actionHistoryAdmin)
--- backend/hospital/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('reset/', views.reset, name='reset'),
-    path('performAction/', views.performAction, name='performAction'),
-    path('', views.index, name='hospitalIndex'),
-]
