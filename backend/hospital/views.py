954c8cd48736649aae6806f800dadd0103538b3b
Subhajyoti Halder <75578800+subhu-darkknight72@users.noreply.github.com>
2024-04-09 05:58:46 +0530

    connected with env_world file

--- backend/hospital/views.py
+++ backend/hospital/views.py
@@ -6,6 +6,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 
+from .env_world import Game
+
 initial_action = 'initialise'
 initial_observation = 'You are in the hallway. This room is called the hallway. This is the main corridor in the ground floor from which we can go to other rooms. The rooms that we can go from here are : doctor chamber #1, doctor chamber #2, general ward, icu ward, common toilet, Nurse Station. The room contains : wall poster, doctor #1, agent.'
 
@@ -15,12 +17,34 @@ class performAction(APIView):
         # get all record from the actionHistory table
         actions = actionHistory.objects.all()
         serializer = actionHistorySerializer(actions, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        past_actions = serializer.data
+
+        cmds = [action['action'] for action in past_actions[1:]]
+        game = Game()
+        next_action = game.start(cmds)
+        next_action["action_space"].append("-")
+
+        response_data = {
+            "past_actions": past_actions,
+            "next_actions": next_action["action_space"]
+        }
+
+        return Response(response_data, status=status.HTTP_200_OK)
     
     def post(self, request, *args, **kwargs):
         # create a new record in the actionHistory table
         action = request.data.get('action')
-        observation = request.data.get('observation')
+
+        actions = actionHistory.objects.all()
+        serializer = actionHistorySerializer(actions, many=True)
+        past_actions = serializer.data
+        cmds = [action['action'] for action in past_actions[1:]]
+        cmds.append(action)
+        game = Game()
+        next_action = game.start(cmds)
+
+        observation = next_action["observation"]
+        
         actionHistory.objects.create(action=action, observation=observation)
         return Response(status=status.HTTP_201_CREATED)
 
