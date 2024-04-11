069f1b09dfe73a29905f6dd9fe0727d6f3d52453
Subhajyoti Halder <75578800+subhu-darkknight72@users.noreply.github.com>
2024-04-11 15:41:06 +0530

    Fixed the LLM for action recomentation

--- backend/hospital/model/react_reflexion_custom_hospital.py
+++ backend/hospital/model/react_reflexion_custom_hospital.py
@@ -10,56 +10,92 @@ Original file is located at
 """### Language Model"""
 
 import os
+import json
+import sys
 from openai import AzureOpenAI
 
+
+from dotenv import load_dotenv
+load_dotenv()
+
 client = AzureOpenAI(
-  azure_endpoint = "https://cnerg-gpt-france.openai.azure.com/",
-  api_key="06cf45fb5c194e76805320b11bccf539",
-  api_version="2023-05-15"
+  	azure_endpoint = os.getenv('AZUREOPENAI_API_BASE'),
+  	api_key=os.getenv('AZUREOPENAI_API_KEY'),
+  	api_version=os.getenv('AZUREOPENAI_API_VERSION')
 )
 
 deployment_name='GPT-4-France'
 
 
 def llm(prompt, stop=['\n']):
-  response = client.chat.completions.create(
-    model="GPT-4-France",
-    messages=[
-                {"role": "system", "content": "You are a helpful AI robot inside a hospital environment."},
-                {"role": "user", "content": prompt} ],
-    temperature=0,
-    top_p=0.98,
-    frequency_penalty=0,
-    presence_penalty=0,
-    stop=stop)
-  return response.choices[0].message.content
+	response = client.chat.completions.create(
+		model="GPT-4-France",
+		messages=[
+			{"role": "system", "content": "You are a helpful AI robot inside a hospital environment."},
+			{"role": "user", "content": prompt}],
+		temperature=0,
+		top_p=0.98,
+		frequency_penalty=0,
+		presence_penalty=0,
+		stop=stop)
+	return response.choices[0].message.content
 
-import json
 prompt_file = 'react_waste_2.json'
 with open(prompt_file, 'r') as f:
     d = json.load(f)
 
-prompt = 'Interact with the hospital environment to solve a task. Here are two examples.\n' + d[f'react_waste_1'] + d[f'react_waste_2'] + '\nHere is the task.\n'
+init_prompt = 'Interact with the hospital environment to solve a task. Here are two examples.\n' + d[f'react_waste_1'] + d[f'react_waste_2'] + '\nHere is the task.\n'
 # Evaluate variation-4
 ob = "You are in the hallway. This is the main corridor in the ground floor from which we can go to other rooms. The rooms that we can go from here are : doctor chamber, general ward, common toilet, hallway nurse station. The room contains : wall poster, doctor #1. \nYour task is to : find all the waste products from the doctor chamber and put them on dustbin."
-init_prompt = prompt + ob + '\n>'
+init_prompt = init_prompt + ob + '\n>'
 prompt = ''
 
-import sys
+filename = 'prompts.txt'
+# with open(filename , 'w') as f:
+# 	f.write(init_prompt+'\n')
 
-for i in range(1, 50):
-  action = llm(init_prompt + prompt, stop=['\n']).strip()
-  print(f"output= {action}")
-  if action.startswith('think:'):
-    observation = 'OK.'
-  else:
-    observation=input("obs= ")
+# i=0
+# while True:
+# 	action = llm(init_prompt + prompt, stop=['\n']).strip()
+# 	while action.startswith('think:'):
+# 		observation = 'OK.'
+# 		prompt += f' {action}\n{observation}\n>'
+# 		new_prompt = f' {action}\n{observation}\n>'
+# 		action = llm(init_prompt + prompt, stop=['\n']).strip()
+# 		with open(filename , 'a') as f:
+# 			f.write(new_prompt+'\n')
+	
+# 	print(f"output= {action}")
+# 	observation=input("obs= ")
 
-  print(f'Act {i}: {action}\nObs {i}: {observation}')
-  sys.stdout.flush()
-  prompt += f' {action}\n{observation}\n>'
+# 	i+=1
+# 	new_prompt = f' {action}\n{observation}\n>'
+# 	prompt += f' {action}\n{observation}\n>'
 
+# 	with open(filename , 'a') as f:
+# 		f.write(new_prompt+'\n')
 
+while True:
+	# read prompt from file
+	with open(filename , 'r') as f:
+		prompt = f.read()
+	
+	action = llm(prompt, stop=['\n']).strip()
+	# check if action contains 'think:' in any form
+	while action.find('think:') != -1:
+		observation = 'OK.'
+		prompt += f' {action}\n{observation}\n>'
+		new_prompt = f' {action}\n{observation}\n>'
+		action = llm(prompt, stop=['\n']).strip()
+		with open(filename , 'a') as f:
+			f.write(new_prompt)
+	
+	print(f"output= {action}" )
+	observation=input("obs= ")
 
+	new_prompt = f' {action}\n{observation}\n>'
+	# write prompt to file
+	with open (filename , 'a') as f:
+		f.write(new_prompt)
 
-
+	
